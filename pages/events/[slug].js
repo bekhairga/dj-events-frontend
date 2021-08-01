2984bd4890e3e771241bdcb209dfec078ecf66f5
BekhaIrga <irgashevbehzod@gmail.com>
2021-08-01 07:20:22 +0500

    Next js installed and basic way of creating pages shown

---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function EventPage() {
+	return (
+		<div>
+			<h1>Event-Page</h1>
+		</div>
+	);
+}
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+export default function EventsPage() {
+	return (
+		<div>
+			<h1>This is events page</h1>
+		</div>
+	);
+}
