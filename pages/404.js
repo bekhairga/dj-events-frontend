a35c12218dda110fece9e8a1708989cf5dd3ae09
BekhaIrga <irgashevbehzod@gmail.com>
2021-08-01 09:56:14 +0500

    Added jsconfig.json where added functionality to use @/component or @/styles import and also refactored imports for the new setup

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
-import Layout from '../components/layout';
-import styles from '../styles/404.module.css';
+import Layout from '@/components/layout';
+import styles from '@/styles/404.module.css';
 import Link from 'next/link';
 import { FaExclamationTriangle } from 'react-icons/fa';
 
