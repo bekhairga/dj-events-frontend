a6fde18a04719a506bc7c2699b9db716b06bfe96
BekhaIrga <irgashevbehzod@gmail.com>
2021-08-01 07:35:36 +0500

    A way to use links and a way to get slug name or id

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,9 @@
+import Link from 'next/link';
 export default function AboutPage() {
 	return (
 		<div>
 			<h1>About</h1>
+			<Link href='/'>Home</Link>
 			<p>This is an app to find the latest DJ and other music events</p>
 			<p>Version: 1.0.0</p>
 		</div>
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,12 @@
+import { useRouter } from 'next/router';
 export default function EventPage() {
+	const router = useRouter();
+	console.log(router);
 	return (
 		<div>
 			<h1>Event-Page</h1>
+			<p>{router.query.slug}</p>
+			<button onClick={() => router.push('/')}>Redirect to HomePage</button>
 		</div>
 	);
 }
