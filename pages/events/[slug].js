6533a53894002a8a1f50c7d4226f1d34f37d7e8a
BekhaIrga <irgashevbehzod@gmail.com>
2021-08-03 11:26:02 +0500

    Added styles to slug page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,46 @@
 import { useRouter } from 'next/router';
 import Layout from '@/components/Layout';
 import { API_URL } from './../../config/index';
+import styles from '@/styles/Event.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
 export default function EventPage({ evt }) {
+	const deleteEvent = () => {};
 	return (
 		<Layout>
-			<h1>{evt.name}</h1>
+			<div className={styles.event}>
+				<div className={styles.controls}>
+					<Link href={`/events/edit/${evt.id}`}>
+						<a>
+							<FaPencilAlt />
+							Edit Event
+						</a>
+					</Link>
+					<a href='#' className={styles.delete} onClick={deleteEvent}>
+						<FaTimes />
+						Delete
+					</a>
+				</div>
+				<span>
+					{evt.data} at {evt.time}
+				</span>
+				<h1>{evt.name}</h1>
+				{evt.image && (
+					<div className={styles.image}>
+						<Image src={evt.image} width={960} height={600} />
+					</div>
+				)}
+				<h3>Performers: </h3>
+				<p>{evt.performers}</p>
+				<h3>Description</h3>
+				<p>{evt.description}</p>
+				<h3>Venue: {evt.venue}</h3>
+				<p>{evt.address}</p>
+				<Link href={'/events'}>
+					<a className={styles.back}>{'>'} Go Back</a>
+				</Link>
+			</div>
 		</Layout>
 	);
 }
