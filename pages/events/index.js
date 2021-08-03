e4f3fe502b2188b487f71462af7cf255b8bb1735
BekhaIrga <irgashevbehzod@gmail.com>
2021-08-03 10:51:49 +0500

    Added events to home page and created events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,40 @@
+import Link from 'next/link';
 import Layout from '@/components/Layout';
-export default function EventsPage() {
+import { API_URL } from '@config/index';
+import EventItem from '@/components/EventItem';
+//this is being rendered on client;
+export default function EventsPage({ events }) {
 	return (
-		<Layout title='All Events'>
-			<h1>This is events page</h1>
+		<Layout>
+			<h1>Events</h1>
+			{events.length === 0 ? (
+				<h3>No events to show</h3>
+			) : (
+				events.map((evt) => <EventItem key={evt.id} evt={evt} />)
+			)}
 		</Layout>
 	);
 }
+//this is being rendered on server;
+//below is a function that runs on serve time
+export async function getServerSideProps() {
+	const res = await fetch(`${API_URL}/api/events`);
+	const events = await res.json();
+	return {
+		props: {
+			events,
+		},
+	};
+}
+//below is a function that runs only at build time
+// export async function getStaticProps() {
+// 	const res = await fetch(`${API_URL}/api/events`);
+// 	const events = await res.json();
+// 	return {
+// 		props: {
+// 			events,
+// //revalidate is when to rebuilt that page in a number of seconds when the props are changed
+// 			revalidate: 1,
+// 		},
+// 	};
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import Link from 'next/link';
 import Layout from '@/components/Layout';
 import { API_URL } from '@config/index';
+import EventItem from '@/components/EventItem';
 //this is being rendered on client;
 export default function HomePage({ events }) {
 	return (
 		<Layout>
 			<h1>Upcoming events</h1>
-			<Link href='/about'>About</Link>
+			{events.length === 0 ? (
+				<h3>No events to show</h3>
+			) : (
+				events.map((evt) => <EventItem key={evt.id} evt={evt} />)
+			)}
+			{events.length > 0 && (
+				<Link href='/events'>
+					<a className='btn btn-secondary'>View All Events</a>
+				</Link>
+			)}
 		</Layout>
 	);
 }
@@ -17,7 +27,7 @@ export async function getServerSideProps() {
 	const events = await res.json();
 	return {
 		props: {
-			events,
+			events: events.slice(0, 3),
 		},
 	};
 }
