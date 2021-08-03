972af800dbf620357cc616f25de9b55ff39b2b74
BekhaIrga <irgashevbehzod@gmail.com>
2021-08-03 11:13:03 +0500

    A way to render dynamic pages dependent on id or slug using getStaticPaths and getStaticProps

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,13 +1,48 @@
 import { useRouter } from 'next/router';
 import Layout from '@/components/Layout';
-export default function EventPage() {
-	const router = useRouter();
-	console.log(router);
+import { API_URL } from './../../config/index';
+export default function EventPage({ evt }) {
 	return (
 		<Layout>
-			<h1>Event-Page</h1>
-			<p>{router.query.slug}</p>
-			<button onClick={() => router.push('/')}>Redirect to HomePage</button>
+			<h1>{evt.name}</h1>
 		</Layout>
 	);
+}
+//it is not static
+// export async function getServerSideProps({ query: { slug } }) {
+// 	const res = await fetch(`${API_URL}/api/events/${slug}`);
+// 	const events = await res.json();
+// 	return {
+// 		props: {
+// 			evt: events[0],
+// 		},
+// 	};
+// }
+
+export async function getStaticPaths() {
+	const res = await fetch(`${API_URL}/api/events`);
+	const events = await res.json();
+	const paths = events.map((evt) => {
+		return {
+			params: {
+				slug: evt.slug,
+			},
+		};
+	});
+	return {
+		paths,
+		fallback: true,
+	};
+}
+
+//it is static
+export async function getStaticProps({ params: { slug } }) {
+	const res = await fetch(`${API_URL}/api/events/${slug}`);
+	const events = await res.json();
+	return {
+		props: {
+			evt: events[0],
+		},
+		revalidate: 1,
+	};
 }
