e8fa358be5ec282e753d35d246ffece906f2d0a3
BekhaIrga <irgashevbehzod@gmail.com>
2021-08-03 17:35:42 +0500

    Created Search component that get user input and uses api to search data

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import styles from '@/styles/Header.module.css';
+import Search from '@/components/Search';
 export default function Header() {
 	return (
 		<header className={styles.header}>
@@ -8,6 +9,7 @@ export default function Header() {
 					<a>DJ Events</a>
 				</Link>
 			</div>
+			<Search />
 			<nav>
 				<ul>
 					<li>
