552934ee43f7265052586994622eff68699424c5
BekhaIrga <irgashevbehzod@gmail.com>
2021-08-05 22:03:56 +0500

    Added delete event functionality

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,12 +1,30 @@
-import { useRouter } from 'next/router';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Layout from '@/components/Layout';
 import { API_URL } from './../../config/index';
 import styles from '@/styles/Event.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+import { useRouter } from 'next/router';
 export default function EventPage({ evt }) {
-	const deleteEvent = () => {};
+	const router = useRouter();
+	const deleteEvent = async () => {
+		if (confirm('Are you sure?')) {
+			const res = await fetch(`${API_URL}/events/${evt.id}`, {
+				method: 'DELETE',
+			});
+			const data = await res.json();
+			if (!res.ok) {
+				toast.error(data.message);
+			} else {
+				toast.success('DELETED');
+				setTimeout(() => {
+					router.push('/events');
+				}, 1000);
+			}
+		}
+	};
 	return (
 		<Layout>
 			<div className={styles.event}>
@@ -23,9 +41,10 @@ export default function EventPage({ evt }) {
 					</a>
 				</div>
 				<span>
-					{new Date(evt.data).toLocaleDateString('en-US')} at {evt.time}
+					{new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
 				</span>
 				<h1>{evt.name}</h1>
+				<ToastContainer />
 				{evt.image && (
 					<div className={styles.image}>
 						<Image src={evt.image.formats.large.url} width={960} height={600} />
